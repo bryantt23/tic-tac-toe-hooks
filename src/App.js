93bcb8a54d2d7f3c5422797937c36915b725a1bd
bryant tunbutr <bryantt23@users.noreply.github.com>
2021-05-30 18:00:10 -0700

    Move GameManager to a separate file

--- src/App.js
+++ src/App.js
@@ -1,57 +1,26 @@
 import Board from './Board';
 import React, { useState, useEffect } from 'react';
+import GameManager from './GameManager';
 
-const size = 3;
-function initializeBoard() {
-  const boardState = [];
-  for (let i = 0; i < size; i++) {
-    let row = [];
-    for (let j = 0; j < size; j++) {
-      row.push({ row: i, col: j, mark: '_' });
-    }
-    boardState.push(row);
-  }
-  return boardState;
-}
+const gameManager = new GameManager();
 
-let playerTurn = 0;
 function App() {
   const [gameHasWinner, setGameHasWinner] = useState(false);
   const [board, setBoard] = useState(null);
   const [message, setMessage] = useState('');
   const [boardHistory, setBoardHistory] = useState([]);
 
-  function whichPlayerTurn() {
-    return playerTurn % 2 == 0 ? 'X' : 'O';
-  }
-
   function changeBoardState(index) {
-    console.log(
-      '🚀 ~ file: App.js ~ line 29 ~ changeBoardState ~ index',
-      index
-    );
-    console.log(
-      '🚀 ~ file: App.js ~ line 37 ~ changeBoardState ~ playerTurn',
-      playerTurn
-    );
-
-    // if (gameHasWinner && index === playerTurn) {
-    //   return;
-    // }
-
     const boardAtIndex = JSON.parse(JSON.stringify(boardHistory[index]));
     setBoard(boardAtIndex);
-    // setGameHasWinner(false);
-    nextPlayerTurn(boardAtIndex);
-    playerTurn = index + 1;
-    // const messageLocal = `It is ${whichPlayerTurn()}'s turn`;
-    // setMessage(messageLocal);
+    gameManager.setPlayerTurn(index);
+    gameManager.nextPlayerTurn(boardAtIndex, setGameHasWinner, setMessage);
   }
 
   useEffect(() => {
-    const boardState = initializeBoard();
+    const boardState = gameManager.initializeBoard();
     setBoard(boardState);
-    setMessage(`It is ${whichPlayerTurn()}'s turn`);
+    setMessage(`It is ${gameManager.whichPlayerTurn()}'s turn`);
     setBoardHistory([]);
   }, []);
 
@@ -60,20 +29,22 @@ function App() {
   }
 
   const callBack = (row, col) => {
-    // debugger;
-    if (isValidMove(row, col) && !hasWinner(board)) {
+    if (
+      isValidMove(row, col) &&
+      !gameManager.hasWinner(board, setGameHasWinner)
+    ) {
       let boardHistoryCopy = JSON.parse(JSON.stringify(boardHistory));
 
       const boardCopy = [...board];
-      boardCopy[row][col].mark = whichPlayerTurn();
+      boardCopy[row][col].mark = gameManager.whichPlayerTurn();
 
       if (boardHistoryCopy.length === 0) {
         boardHistoryCopy[0] = [...boardCopy];
       } else {
-        boardHistoryCopy.splice(playerTurn);
+        boardHistoryCopy.splice(gameManager.getPlayerTurn());
         boardHistoryCopy = [...boardHistoryCopy, boardCopy];
       }
-      nextPlayerTurn(boardCopy);
+      gameManager.nextPlayerTurn(boardCopy, setGameHasWinner, setMessage);
       setBoard(boardCopy);
       setBoardHistory(boardHistoryCopy);
 
@@ -81,106 +52,13 @@ function App() {
         '🚀 ~ file: App.js ~ line 71 ~ callBack ~ boardHistoryCopy',
         boardHistoryCopy
       );
-      // boardHistoryCopy = [...boardHistoryCopy, boardCopy];
     }
   };
 
-  function nextPlayerTurn(board) {
-    let messageLocal;
-    if (hasWinner(board)) {
-      messageLocal = whichPlayerTurn() + ' has won!';
-      // view.disableBoard();//TODO
-      // let element = document.getElementById('board');
-      // element.style = {
-      //   pointerEvents: 'none',
-      //   opacity: '0.4'
-      // };
-    } else {
-      playerTurn++;
-      if (playerTurn === 9) {
-        messageLocal = "It's a draw!";
-        // view.disableBoard();//TODO
-      } else {
-        messageLocal = `It is ${whichPlayerTurn()}'s turn`;
-      }
-    }
-    setMessage(messageLocal);
-  }
-
-  function hasWinner(board) {
-    const winner =
-      horizontalWinner(board) || verticalWinner(board) || diagonalWinner(board);
-    if (winner) {
-      setGameHasWinner(true);
-    }
-    return winner;
-  }
-
-  function horizontalWinner(board) {
-    for (let i = 0; i < size; i++) {
-      let ct = 0;
-      for (let j = 0; j < size; j++) {
-        if (board[i][j].mark === 'X') {
-          ct++;
-        } else if (board[i][j].mark === 'O') {
-          ct--;
-        }
-      }
-      if (ct === 3 || ct === -3) {
-        return true;
-      }
-    }
-    return false;
-  }
-
-  function verticalWinner(board) {
-    for (let j = 0; j < size; j++) {
-      let ct = 0;
-      for (let i = 0; i < size; i++) {
-        if (board[i][j].mark === 'X') {
-          ct++;
-        } else if (board[i][j].mark === 'O') {
-          ct--;
-        }
-      }
-      if (ct === 3 || ct === -3) {
-        return true;
-      }
-    }
-    return false;
-  }
-
-  function diagonalWinner(board) {
-    let ct = 0;
-    for (let i = 0; i < size; i++) {
-      if (board[i][i].mark === 'X') {
-        ct++;
-      } else if (board[i][i].mark === 'O') {
-        ct--;
-      }
-    }
-    if (ct === 3 || ct === -3) {
-      return true;
-    }
-
-    ct = 0;
-    for (let i = 0; i < size; i++) {
-      if (board[i][size - i - 1].mark === 'X') {
-        ct++;
-      } else if (board[i][size - i - 1].mark === 'O') {
-        ct--;
-      }
-    }
-    if (ct === 3 || ct === -3) {
-      return true;
-    }
-  }
-
   return (
     <div>
-      <p>{playerTurn}</p>
+      <p>{gameManager.getPlayerTurn()}</p>
       {message}
-      {/* {JSON.stringify(boardHistory)} */}
       <Board board={board} callBack={callBack} />
       <div>
         {boardHistory &&
---
+++ src/GameManager.js
@@ -0,0 +1,121 @@
+const size = 3;
+let playerTurn = 0;
+
+export default function GameManager() {
+  function initializeBoard() {
+    const boardState = [];
+    for (let i = 0; i < size; i++) {
+      let row = [];
+      for (let j = 0; j < size; j++) {
+        row.push({ row: i, col: j, mark: '_' });
+      }
+      boardState.push(row);
+    }
+    return boardState;
+  }
+
+  function hasWinner(board, setGameHasWinner) {
+    const winner =
+      horizontalWinner(board) || verticalWinner(board) || diagonalWinner(board);
+    if (winner) {
+      setGameHasWinner(true);
+    }
+    return winner;
+  }
+
+  function horizontalWinner(board) {
+    for (let i = 0; i < size; i++) {
+      let ct = 0;
+      for (let j = 0; j < size; j++) {
+        if (board[i][j].mark === 'X') {
+          ct++;
+        } else if (board[i][j].mark === 'O') {
+          ct--;
+        }
+      }
+      if (ct === 3 || ct === -3) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  function verticalWinner(board) {
+    for (let j = 0; j < size; j++) {
+      let ct = 0;
+      for (let i = 0; i < size; i++) {
+        if (board[i][j].mark === 'X') {
+          ct++;
+        } else if (board[i][j].mark === 'O') {
+          ct--;
+        }
+      }
+      if (ct === 3 || ct === -3) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  function diagonalWinner(board) {
+    let ct = 0;
+    for (let i = 0; i < size; i++) {
+      if (board[i][i].mark === 'X') {
+        ct++;
+      } else if (board[i][i].mark === 'O') {
+        ct--;
+      }
+    }
+    if (ct === 3 || ct === -3) {
+      return true;
+    }
+
+    ct = 0;
+    for (let i = 0; i < size; i++) {
+      if (board[i][size - i - 1].mark === 'X') {
+        ct++;
+      } else if (board[i][size - i - 1].mark === 'O') {
+        ct--;
+      }
+    }
+    if (ct === 3 || ct === -3) {
+      return true;
+    }
+  }
+
+  function nextPlayerTurn(board, setGameHasWinner, setMessage) {
+    let messageLocal;
+    if (hasWinner(board, setGameHasWinner)) {
+      messageLocal = whichPlayerTurn() + ' has won!';
+    } else {
+      playerTurn++;
+      if (playerTurn === 9) {
+        messageLocal = "It's a draw!";
+      } else {
+        messageLocal = `It is ${whichPlayerTurn()}'s turn`;
+      }
+    }
+    setMessage(messageLocal);
+  }
+
+  function whichPlayerTurn() {
+    return playerTurn % 2 == 0 ? 'X' : 'O';
+  }
+
+  function getPlayerTurn() {
+    return playerTurn;
+  }
+
+  function setPlayerTurn(turn) {
+    playerTurn = turn;
+  }
+
+  return {
+    initializeBoard,
+    hasWinner,
+    nextPlayerTurn,
+    whichPlayerTurn,
+    getPlayerTurn,
+    setPlayerTurn
+  };
+}
